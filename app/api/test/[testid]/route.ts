526a92cef214b8b868ff5fee3f9bdc7e3fb4c79b
Saawn <itissaavn@gmail.com>
2024-06-07 15:51:49 +0530

    add: setup basic api with hono

--- app/api/test/[testid]/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import test from "node:test";
-
-export const GET = (
-  request: NextRequest,
-  { params }: { params: { testid: string } }
-) => {
-  return NextResponse.json({ hello: "world", testid: params.testid });
-};
