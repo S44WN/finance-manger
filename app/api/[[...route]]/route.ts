c9a5bb0a73af0dadbc50939cfdf28460719d39b8
Saawn <itissaavn@gmail.com>
2024-06-07 23:58:10 +0530

    add: basic middlewar - zod for validation

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,15 +1,43 @@
+import z from "zod";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
+import { zValidator } from "@hono/zod-validator";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app.get("/hello", (c) => {
-  return c.json({
-    message: "Hello Next.js!",
-  });
-});
+app
+  .get("/hello", (c) => {
+    return c.json({
+      message: "Hello Next.js!",
+    });
+  })
+
+  .get(
+    "/hello/:name", // can be accessed via c.req.params
+
+    /* 
+    zValidator - validates the request parameters
+    - The first argument is the name of the request parameter
+    - The second argument is the schema to validate the request parameter
+    */
+
+    // middleware
+    zValidator(
+      "param",
+      z.object({
+        name: z.string(),
+      })
+    ),
+    (c) => {
+      const { name } = c.req.valid("param"); // .valid - returns the validated request parameter
+
+      return c.json({
+        message: `Hello ${name}!`,
+      });
+    }
+  );
 
 export const GET = handle(app);
 export const POST = handle(app);
