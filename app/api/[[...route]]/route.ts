d5df4bf4f90d7a5e21834354dc1fe27e86225e63
Saawn <itissaavn@gmail.com>
2024-06-08 21:48:04 +0530

    add: clerk middleware in hono

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,70 +2,26 @@ import z from "zod";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import { zValidator } from "@hono/zod-validator";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app
-  .get("/hello", (c) => {
+app.get("/hello", clerkMiddleware(), (c) => {
+  const auth = getAuth(c); // get the user from the request object
+
+  if (!auth?.userId) {
     return c.json({
-      message: "Hello Next.js!",
+      error: "User not authenticated",
     });
-  })
+  }
 
-  .get(
-    "/hello/:name", // can be accessed via c.req.params
-
-    /* 
-    zValidator - validates the request parameters
-    - The first argument is the name of the request parameter
-    - The second argument is the schema to validate the request parameter
-    */
-
-    // middleware
-    zValidator(
-      "param",
-      z.object({
-        name: z.string(),
-      })
-    ),
-    (c) => {
-      const { name } = c.req.valid("param"); // .valid - returns the validated request parameter
-
-      return c.json({
-        message: `Hello ${name}!`,
-      });
-    }
-  )
-
-  .post(
-    "/create/:userId",
-
-    zValidator(
-      "json",
-      z.object({
-        name: z.string(),
-        email: z.string().email(),
-      })
-    ),
-
-    zValidator(
-      "param",
-      z.object({
-        userId: z.string(),
-      })
-    ),
-
-    (c) => {
-      const { name, email } = c.req.valid("json");
-      const { userId } = c.req.valid("param");
-
-      return c.json({
-        message: "User created!",
-      });
-    }
-  );
+  return c.json({
+    message: "Hello Next.js!",
+    userid: auth.userId,
+  });
+});
 
 export const GET = handle(app);
 export const POST = handle(app);
