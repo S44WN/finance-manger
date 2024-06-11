5dd1c79b29a02e60fcc81d6ff9b52bc602029842
Saawn <itissaavn@gmail.com>
2024-06-11 23:03:53 +0530

    Add: welcome section and fixes for header

--- components/header.tsx
+++ components/header.tsx
@@ -3,10 +3,11 @@ import { UserButton, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
 import { HeaderLogo } from "@/components/header-logo";
 import { Navigation } from "@/components/navigation";
 import { Loader2 } from "lucide-react";
+import { WelcomeMsg } from "@/components/welcome-msg";
 
 export const Header = () => {
   return (
-    <header className="bg-gradient-to-b from-blue-500 to to-blue-300 px-4 py-8 lg:px-14 pb-36">
+    <header className="bg-gradient-to-b from-blue-600 to to-blue-400 px-4 py-8 lg:px-14 pb-36">
       <div className="max-w-screen-2xl mx-auto">
         <div className="w-full flex items-center justify-between mb-14">
           <div className="flex items-center lg:gap-x-16">
@@ -20,6 +21,7 @@ export const Header = () => {
             <Loader2 className="size-8 animate-spin text-slate-400" />
           </ClerkLoading>
         </div>
+        <WelcomeMsg />
       </div>
     </header>
   );
