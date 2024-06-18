3731e5f7500da4f285d4bcae728dda815140550b
Saawn <itissaavn@gmail.com>
2024-06-19 00:03:20 +0530

    init accounts api in hono

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,11 +1,13 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
+import accounts from "./accounts";
+
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app.get("/hello", (c) => c.json("Hello, world!"));
+app.route("/accounts", accounts);
 
 export const GET = handle(app);
 export const POST = handle(app);
