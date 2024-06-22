8b5f76080c823d77536ad586a8d676888f423f22
Saawn <itissaavn@gmail.com>
2024-06-22 23:44:39 +0530

    add sample query

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+
 export default function Home() {
+  const { data: accounts } = useGetAccounts();
+
   return (
     <div>
-      <h1>dashboard</h1>
+      <h1>Accounts</h1>
+      <ul>
+        {accounts?.map((account) => (
+          <li key={account.id}>{account.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
